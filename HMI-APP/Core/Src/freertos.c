ef2433f6ec02b82e760be096976dce465e39543c
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-27 08:33:28 +0700

    Touchgfx using single buffer

--- HMI-APP/Core/Src/freertos.c
+++ HMI-APP/Core/Src/freertos.c
@@ -54,17 +54,7 @@
 /* USER CODE END FunctionPrototypes */
 
 /* Hook prototypes */
-void vApplicationStackOverflowHook(xTaskHandle xTask, signed char *pcTaskName);
 void vApplicationMallocFailedHook(void);
-
-/* USER CODE BEGIN 4 */
-__weak void vApplicationStackOverflowHook(xTaskHandle xTask, signed char *pcTaskName)
-{
-   /* Run time stack overflow checking is performed if
-   configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2. This hook function is
-   called if a stack overflow is detected. */
-}
-/* USER CODE END 4 */
 
 /* USER CODE BEGIN 5 */
 __weak void vApplicationMallocFailedHook(void)
