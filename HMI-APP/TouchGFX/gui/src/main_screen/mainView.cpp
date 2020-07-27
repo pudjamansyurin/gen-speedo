f0e9a36cb3eaed48e7ed979dfbb71170f5437342
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-27 11:14:25 +0700

    TouchGFX clock that works

--- HMI-APP/TouchGFX/gui/src/main_screen/mainView.cpp
+++ HMI-APP/TouchGFX/gui/src/main_screen/mainView.cpp
@@ -1,7 +1,7 @@
-#include <gui/main_screen/MainView.hpp>
+#include <gui/main_screen/mainView.hpp>
 
 mainView::mainView() :
-    tickCounter(0)
+        tickCounter(0)
 {
 }
 
@@ -27,28 +27,28 @@ void mainView::handleTickEvent()
     tickCounter++;
 
     if (tickCounter % 60 == 0)
-    {
+            {
         if (++digitalSeconds >= 60)
-        {
+                {
             digitalSeconds = 0;
             if (++digitalMinutes >= 60)
-            {
+                    {
                 digitalMinutes = 0;
                 if (++digitalHours >= 24)
-                {
+                        {
                     digitalHours = 0;
                 }
             }
         }
 
         if (++analogSeconds >= 60)
-        {
+                {
             analogSeconds = 0;
             if (++analogMinutes >= 60)
-            {
+                    {
                 analogMinutes = 0;
                 if (++analogHours >= 24)
-                {
+                        {
                     analogHours = 0;
                 }
             }
