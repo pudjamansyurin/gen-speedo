2cca371ed036c0f37e0617121525d21f4779b0cd
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-07-30 10:54:30 +0700

    Change needle interpolation and alpha value

--- HMI-APP/TouchGFX/generated/gui_generated/src/screen1_screen/Screen1ViewBase.cpp
+++ HMI-APP/TouchGFX/generated/gui_generated/src/screen1_screen/Screen1ViewBase.cpp
@@ -52,7 +52,8 @@ Screen1ViewBase::Screen1ViewBase()
     speedoMeter.setOrigo(100.000f, 100.000f, 1000.000f);
     speedoMeter.setCamera(100.000f, 100.000f);
     speedoMeter.updateAngles(0.000f, 0.000f, -1.570f);
-    speedoMeter.setRenderingAlgorithm(touchgfx::TextureMapper::NEAREST_NEIGHBOR);
+    speedoMeter.setRenderingAlgorithm(touchgfx::TextureMapper::BILINEAR_INTERPOLATION);
+    speedoMeter.setAlpha(163);
 
     add(background);
     add(seinLeft);
