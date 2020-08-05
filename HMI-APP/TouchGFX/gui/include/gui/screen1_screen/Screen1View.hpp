ed36b19fe31acda86ca928573a57ee217a21e858
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-08-05 17:00:24 +0700

    Added logic

--- HMI-APP/TouchGFX/gui/include/gui/screen1_screen/Screen1View.hpp
+++ HMI-APP/TouchGFX/gui/include/gui/screen1_screen/Screen1View.hpp
@@ -4,6 +4,16 @@
 #include <gui_generated/screen1_screen/Screen1ViewBase.hpp>
 #include <gui/screen1_screen/Screen1Presenter.hpp>
 
+typedef struct {
+    struct {
+        char mode[2][6];
+        char unit[2][11];
+    } report;
+    struct {
+        char mode[4][9];
+    } drive;
+} collection_t;
+
 class Screen1View : public Screen1ViewBase
 {
 public:
@@ -11,7 +21,15 @@ public:
     virtual ~Screen1View() {}
     virtual void setupScreen();
     virtual void tearDownScreen();
+
+    virtual void handleTickEvent();
 protected:
+    uint32_t ticker;
+    uint32_t _tripValue;
+    uint32_t _engineValue;
+    uint8_t _batteryValue;
+    uint8_t _signalValue;
+    uint8_t _speedValue;
 };
 
 #endif // SCREEN1VIEW_HPP
--- HMI-APP/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
+++ HMI-APP/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -1,6 +1,22 @@
 #include <gui/screen1_screen/Screen1View.hpp>
 
-Screen1View::Screen1View()
+//static collection_t COL = {
+//        .report = {
+//                .mode = { "RANGE", "AVG" },
+//                .unit = { "0 KM", "0 KM/KWH" },
+//        },
+//        .drive = {
+//                .mode = { "ECONOMIC", "SPORT", "PERSONAL", "STANDARD" }
+//        }
+//};
+
+Screen1View::Screen1View() :
+        ticker(0),
+        _tripValue(0),
+        _engineValue(0),
+        _batteryValue(0),
+        _signalValue(0),
+        _speedValue(0)
 {
 
 }
@@ -13,4 +29,60 @@ void Screen1View::setupScreen()
 void Screen1View::tearDownScreen()
 {
     Screen1ViewBase::tearDownScreen();
+}
+
+void Screen1View::handleTickEvent() {
+    ticker++;
+
+    if(ticker % 60 == 0) {
+        seinLeft.setVisible(!seinLeft.isVisible());
+        seinRight.setVisible(!seinRight.isVisible());
+        seinLeft.invalidate();
+        seinRight.invalidate();
+    }
+    if(ticker % 50 == 0) {
+        if(_batteryValue > 100) {
+            _batteryValue = 0;
+        } else  {
+            _batteryValue++;
+        }
+
+        Unicode::snprintf(batteryValueBuffer, BATTERYVALUE_SIZE, "%03d", _batteryValue);
+        batteryValue.invalidate();
+    }
+    if(ticker % 30 == 0) {
+        if(_signalValue > 100) {
+            _signalValue = 0;
+        } else  {
+            _signalValue++;
+        }
+
+        Unicode::snprintf(signalValueBuffer, SIGNALVALUE_SIZE, "%03d", _signalValue);
+        signalValue.invalidate();
+    }
+    if(ticker % 10 == 0) {
+        if(_tripValue > 999999) {
+            _tripValue = 0;
+        } else  {
+            _tripValue++;
+        }
+
+        Unicode::snprintf(tripValueBuffer, TRIPVALUE_SIZE, "%06d", _tripValue);
+        tripValue.invalidate();
+    }
+
+    if(ticker % 40 == 0) {
+        if(_speedValue > 140) {
+            _speedValue = 0;
+        } else  {
+            _speedValue++;
+        }
+    }
+    if(ticker % 2 == 0) {
+        if(_engineValue > 9000) {
+            _engineValue = 0;
+        } else  {
+            _engineValue+=50;
+        }
+    }
 }
