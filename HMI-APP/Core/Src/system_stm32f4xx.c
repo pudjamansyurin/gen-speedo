dd5fa67cfae31cee7875c904526a0c0d4f2446f8
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-11-27 10:20:04 +0700

    Testing only

--- HMI-APP/Core/Src/system_stm32f4xx.c
+++ HMI-APP/Core/Src/system_stm32f4xx.c
@@ -89,7 +89,7 @@
 /*!< Uncomment the following line if you need to relocate your vector Table in
  Internal SRAM. */
 /* #define VECT_TAB_SRAM */
-#define VECT_TAB_OFFSET  0x20000 /*!< Vector Table base offset field.
+#define VECT_TAB_OFFSET  0x0000 /*!< Vector Table base offset field.
                                    This value must be a multiple of 0x200. */
 /******************************************************************************/
 
@@ -148,7 +148,7 @@ const uint8_t APBPrescTable[8] = { 0, 0, 0, 0, 1, 2, 3, 4 };
  */
 void SystemInit(void)
 {
-    /* FPU settings ------------------------------------------------------------*/
+	/* FPU settings ------------------------------------------------------------*/
 #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
     SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
   #endif
@@ -157,11 +157,11 @@ void SystemInit(void)
   SystemInit_ExtMemCtl();
 #endif /* DATA_IN_ExtSRAM || DATA_IN_ExtSDRAM */
 
-    /* Configure the Vector Table location add offset address ------------------*/
+	/* Configure the Vector Table location add offset address ------------------*/
 #ifdef VECT_TAB_SRAM
   SCB->VTOR = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM */
 #else
-    SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH */
+	SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH */
 #endif
 }
 
@@ -203,50 +203,50 @@ void SystemInit(void)
  */
 void SystemCoreClockUpdate(void)
 {
-    uint32_t tmp = 0, pllvco = 0, pllp = 2, pllsource = 0, pllm = 2;
+	uint32_t tmp = 0, pllvco = 0, pllp = 2, pllsource = 0, pllm = 2;
 
-    /* Get SYSCLK source -------------------------------------------------------*/
-    tmp = RCC->CFGR & RCC_CFGR_SWS;
+	/* Get SYSCLK source -------------------------------------------------------*/
+	tmp = RCC->CFGR & RCC_CFGR_SWS;
 
-    switch (tmp)
-    {
-        case 0x00: /* HSI used as system clock source */
-            SystemCoreClock = HSI_VALUE;
-            break;
-        case 0x04: /* HSE used as system clock source */
-            SystemCoreClock = HSE_VALUE;
-            break;
-        case 0x08: /* PLL used as system clock source */
+	switch (tmp)
+	{
+		case 0x00: /* HSI used as system clock source */
+			SystemCoreClock = HSI_VALUE;
+			break;
+		case 0x04: /* HSE used as system clock source */
+			SystemCoreClock = HSE_VALUE;
+			break;
+		case 0x08: /* PLL used as system clock source */
 
-            /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N
-             SYSCLK = PLL_VCO / PLL_P
-             */
-            pllsource = (RCC->PLLCFGR & RCC_PLLCFGR_PLLSRC) >> 22;
-            pllm = RCC->PLLCFGR & RCC_PLLCFGR_PLLM;
+			/* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N
+			 SYSCLK = PLL_VCO / PLL_P
+			 */
+			pllsource = (RCC->PLLCFGR & RCC_PLLCFGR_PLLSRC) >> 22;
+			pllm = RCC->PLLCFGR & RCC_PLLCFGR_PLLM;
 
-            if (pllsource != 0)
-                    {
-                /* HSE used as PLL clock source */
-                pllvco = (HSE_VALUE / pllm) * ((RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> 6);
-            }
-            else
-            {
-                /* HSI used as PLL clock source */
-                pllvco = (HSI_VALUE / pllm) * ((RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> 6);
-            }
+			if (pllsource != 0)
+					{
+				/* HSE used as PLL clock source */
+				pllvco = (HSE_VALUE / pllm) * ((RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> 6);
+			}
+			else
+			{
+				/* HSI used as PLL clock source */
+				pllvco = (HSI_VALUE / pllm) * ((RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> 6);
+			}
 
-            pllp = (((RCC->PLLCFGR & RCC_PLLCFGR_PLLP) >> 16) + 1) * 2;
-            SystemCoreClock = pllvco / pllp;
-            break;
-        default:
-            SystemCoreClock = HSI_VALUE;
-            break;
-    }
-    /* Compute HCLK frequency --------------------------------------------------*/
-    /* Get HCLK prescaler */
-    tmp = AHBPrescTable[((RCC->CFGR & RCC_CFGR_HPRE) >> 4)];
-    /* HCLK frequency */
-    SystemCoreClock >>= tmp;
+			pllp = (((RCC->PLLCFGR & RCC_PLLCFGR_PLLP) >> 16) + 1) * 2;
+			SystemCoreClock = pllvco / pllp;
+			break;
+		default:
+			SystemCoreClock = HSI_VALUE;
+			break;
+	}
+	/* Compute HCLK frequency --------------------------------------------------*/
+	/* Get HCLK prescaler */
+	tmp = AHBPrescTable[((RCC->CFGR & RCC_CFGR_HPRE) >> 4)];
+	/* HCLK frequency */
+	SystemCoreClock >>= tmp;
 }
 
 #if defined (DATA_IN_ExtSRAM) && defined (DATA_IN_ExtSDRAM)
