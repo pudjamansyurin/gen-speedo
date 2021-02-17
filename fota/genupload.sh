64bac00492cc3d7cd2ddf5b1107943dfa1f3620b
pujak <pujak@host.docker.internal>
2021-02-17 15:46:35 +0700

    Added windows fota support

--- fota/genupload.sh
+++ fota/genupload.sh
@@ -2,7 +2,7 @@
 
 source .env
 
-SRC_BIN_FILE="../HMI-APP/Release/HMI-APP.bin"
+SRC_BIN_FILE="../${DEVICE}-APP/Release/${DEVICE}-APP.bin"
 APP_BIN_FILE="APP.bin"
 CRC_STR_FILE="CRC.txt"
 CRC_BIN_FILE="CRC.bin"
@@ -11,19 +11,17 @@ CRC_APP_FILE="CRC_APP.bin"
 echo "Files preparing..."
 rm -rf $APP_BIN_FILE $CRC_APP_FILE $CRC_STR_FILE $CRC_BIN_FILE
 cp $SRC_BIN_FILE $APP_BIN_FILE
-#objcopy -I binary -O binary --reverse-bytes=4 $SRC_BIN_FILE "tmp.bin"
 
 echo "Checksum calculating..."
 CRC_RESULT=$(java -jar ./jacksum.jar -a crc32_mpeg2 -E hexup -F "#CHECKSUM" $APP_BIN_FILE)
 echo $CRC_RESULT > $CRC_STR_FILE
-#xxd -r -p $CRC_STR_FILE $CRC_BIN_FILE
-xxd -r -p $CRC_STR_FILE | xxd -e -g4 | xxd -r > $CRC_BIN_FILE
+$XXD -r -p $CRC_STR_FILE | $XXD -e -g4 | $XXD -r > $CRC_BIN_FILE
 
 echo "Combine checksum to binary file..."
 cat $CRC_BIN_FILE $APP_BIN_FILE > $CRC_APP_FILE
 
 echo "FTP binary uploading..."
-curl -T $CRC_APP_FILE ftp://ftp.genmotorcycles.com/hmi/ --user "$USERNAME:$PASSWORD" || exit 1
+$CURL -T $CRC_APP_FILE ftp://ftp.genmotorcycles.com/vcu/ --user "$USERNAME:$PASSWORD" || exit 1
 
 echo "FTP done, ready for FOTA."
 
