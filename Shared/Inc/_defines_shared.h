c5ca81a67eefa3d394952390577e73233977ce43
GEN <GEN@host.docker.internal>
2021-03-15 09:42:47 +0700

    Revert application flash area back

--- Shared/Inc/_defines_shared.h
+++ Shared/Inc/_defines_shared.h
@@ -48,5 +48,6 @@
 #define BKP_END_ADDR                            (BKP_START_ADDR + APP_MAX_SIZE - 1)
 #define SIZE_OFFSET                             (APP_MAX_SIZE - 4)
 #define CHECKSUM_OFFSET                         (APP_MAX_SIZE - 8)
+#define HIN_VALUE                    (*(__IO uint32_t*) (APP_START_ADDR - 4))
 
 #endif /* DEFINES_SHARED_H_ */
