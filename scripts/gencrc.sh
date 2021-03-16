7b19472f1ca5c8bed3e37fe90d6812cdc964e34f
Puja Kusuma <pujakusumae@hotmail.com>
2021-03-16 16:34:24 +0700

    Handle gencrc script error

--- scripts/gencrc.sh
+++ scripts/gencrc.sh
@@ -7,13 +7,13 @@ mkdir dist
 sleep 2
 
 echo "Calculating CRC..."
-CRC_RESULT=$($JACKSUM -a crc32_mpeg2 -E hexup -F "#CHECKSUM" $APP_BIN_FILE)
+CRC_RESULT=$($JACKSUM -a crc32_mpeg2 -E hexup -F "#CHECKSUM" $APP_BIN_FILE) || exit 1
 echo $CRC_RESULT > $CRC_STR_FILE
-$XXD -r -p $CRC_STR_FILE | $XXD -e -g4 | $XXD -r > $CRC_BIN_FILE
+$XXD -r -p $CRC_STR_FILE | $XXD -e -g4 | $XXD -r > $CRC_BIN_FILE || exit 1
 sleep 2
 
 echo "Inserting CRC to bin file..."
-cat $CRC_BIN_FILE $APP_BIN_FILE > $CRC_APP_FILE
+cat $CRC_BIN_FILE $APP_BIN_FILE > $CRC_APP_FILE || exit 1
 sleep 2 
 
 echo "CRC = 0x$CRC_RESULT"
