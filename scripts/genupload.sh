629009760f0f890fcb6e67e4ccdc83a3ec8e5792
Puja Kusuma <pujakusumae@hotmail.com>
2021-03-20 07:17:54 +0700

    Fix ftp uploader dev bug

--- scripts/genupload.sh
+++ scripts/genupload.sh
@@ -3,7 +3,7 @@ source .env
 source "./gencrc.sh"
 
 echo "FTP binary uploading..."
-$CURL -T $CRC_APP_FILE ftp://ftp.genmotorcycles.com/hmi/ --user "$FTP_USERNAME:$FTP_PASSWORD" || exit 1
+$CURL -T $CRC_APP_FILE "ftp://ftp.genmotorcycles.com/$DEV/" --user "$FTP_USERNAME:$FTP_PASSWORD" || exit 1
 
 echo "FTP done, ready for FOTA."
 
